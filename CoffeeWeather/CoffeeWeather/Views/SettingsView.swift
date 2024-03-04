043acf9e67f23632904bbffb34f4eaccca7dc137
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-04 12:02:26 -0600

    settings updates and getting the weather

--- CoffeeWeather/CoffeeWeather/Views/SettingsView.swift
+++ CoffeeWeather/CoffeeWeather/Views/SettingsView.swift
@@ -7,15 +7,50 @@
 
 import UIKit
 
-class SettingsView: UIView {
+final class SettingsView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    private let viewModel: SettingsViewViewModel
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .orange
         translatesAutoresizingMaskIntoConstraints = false
+        addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            ])
     }
 
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    public func configure(with viewModel: SettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    //MARK: - Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
